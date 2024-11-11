0512769c8f1c4f043faf30f2bd90327d2e9b432f
rojebd <roniellberrios@gmail.com>
2024-11-11 16:07:35 -0700

    Updated Config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ set -gx GPG_TTY (tty)
 set fish_greeting
 set -U autovenv_enable yes
 set -U autovenv_announce yes
+set -Ux HAREPATH /usr/src/hare/stdlib:/usr/src/hare/third-party:/home/roniell/sources/hare-raylib
 fish_add_path /home/roniell/.local/bin
--- .emacs.rc/rc.el
+++ .emacs.rc/rc.el
@@ -39,4 +39,35 @@
           (other-window 1)
           (find-file file)))))
 
+(defvar rc/highlight-patterns
+  '("TODO" "FIXME" "XXX" "DONE" "NOTE")
+  "List of regex patterns to highlight.")
+
+(defvar rc/hare-highlight-patterns "[!?]")
+
+(defun rc/hare-setup ()
+  (when (eq major-mode 'hare-mode)
+    (local-set-key (kbd "S-<backspace>") (lambda () (interactive) (delete-backward-char 3)))
+    (hi-lock-mode 1)
+	(highlight-regexp rc/hare-highlight-patterns "hi-red-b")
+    (highlight-regexp (regexp-opt rc/highlight-patterns 'words) "hi-red-b")
+    ;; Reapply highlighting after changes
+    (add-hook 'after-change-functions #'rc/reapply-highlighting nil t)))
+
+(defun rc/python-setup ()
+  (when (eq major-mode 'python-mode)
+	(hi-lock-mode 1)
+    (highlight-regexp (regexp-opt rc/highlight-patterns 'words) "hi-red-b")
+    ;; Reapply highlighting after changes
+    (add-hook 'after-change-functions #'rc/reapply-highlighting nil t)))
+
+
+(defun rc/reapply-highlighting (beg end length)
+  "Reapply the highlighting for TODO, FIXME, and XXX and !? when in hare-mode."
+  (when (eq major-mode 'hare-mode)
+	(highlight-regexp rc/hare-highlight-patterns "hi-red-b")
+	(highlight-regexp (regexp-opt rc/highlight-patterns 'words) "hi-red-b"))
+  ;; Reapply the highlight-regexp for each defined pattern
+  (highlight-regexp (regexp-opt rc/highlight-patterns 'words) "hi-red-b"))
+
 (provide 'rc)
