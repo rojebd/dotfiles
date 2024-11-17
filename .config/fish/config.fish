d9d659434412725fb3836a5426128304cf1d8c6f
rojebd <roniellberrios@gmail.com>
2024-11-17 10:56:40 -0700

    Fish: Aliases to grep, sed, find replaced with rg,fd,sd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,9 @@ set fish_greeting
 set -U autovenv_enable yes
 set -U autovenv_announce yes
 set -Ux HAREPATH /usr/src/hare/stdlib:/usr/src/hare/third-party:/home/roniell/sources/hare-raylib
+
+alias grep="rg $argv"
+alias find="fd $argv"
+alias sed="sd $argv"
+
 fish_add_path /home/roniell/.local/bin
