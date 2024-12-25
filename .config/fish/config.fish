de42089325a7181e2427456b28d672bd96d3c69a
rojebd <roniellberrios@gmail.com>
2024-12-25 00:28:06 -0700

    Fish: Set EDITOR to nvim (Neovim)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting
 set -U autovenv_enable yes
 set -U autovenv_announce yes
 set -Ux HAREPATH /usr/src/hare/stdlib:/usr/src/hare/third-party:/home/roniell/sources/hare-raylib
+set -gx EDITOR nvim
 
 #alias grep="rg $argv"
 #alias find="fd $argv"
