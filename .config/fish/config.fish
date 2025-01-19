e60fa17823d531ee5946a8ce27d7b34d4bf660ef
rojebd <roniellberrios@gmail.com>
2025-01-18 21:48:53 -0700

    Fish: Added hashtable library to custom c include/ld/library path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,9 @@ set -U autovenv_announce yes
 set -x HAREPATH /usr/src/hare/stdlib:/usr/src/hare/third-party:/home/roniell/sources/hare-raylib
 set -x EDITOR nvim
 
-set -x C_INCLUDE_PATH /home/roniell/coding/c/llist/include $C_INCLUDE_PATH
-set -x LIBRARY_PATH /home/roniell/coding/c/llist/lib $LIBRARY_PATH
-set -x LD_LIBRARY_PATH /home/roniell/coding/c/llist/lib $LD_LIBRARY_PATH
+set -x C_INCLUDE_PATH  /home/roniell/coding/c/llist/include /home/roniell/coding/c/hashtable/include $C_INCLUDE_PATH
+set -x LIBRARY_PATH    /home/roniell/coding/c/llist/lib     /home/roniell/coding/c/hashtable/lib     $LIBRARY_PATH
+set -x LD_LIBRARY_PATH /home/roniell/coding/c/llist/lib     /home/roniell/coding/c/hashtable/lib     $LD_LIBRARY_PATH
 
 #alias grep="rg $argv"
 #alias find="fd $argv"
