28606ceb98959c14abf1ec88653fb7ff8a64b2fd
rojebd <roniellberrios@gmail.com>
2025-01-26 15:17:27 -0700

    Qtile: Changed mod key to windows key

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -47,7 +47,7 @@ def startup():
     Popen(expanduser("~/.config/qtile/startup.sh"))
 
 
-mod = "mod1"
+mod = "mod4"
 terminal = "ghostty"
 browser = "firefox"
 launcher = "tofi-drun --drun-launch=true"
