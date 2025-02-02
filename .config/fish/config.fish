5bf86406c7634fea90781219d689662e411c663c
rojebd <roniellberrios@gmail.com>
2025-02-01 18:46:39 -0700

    Fish: Remove HAREPATH Variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,11 @@ set -gx GPG_TTY (tty)
 set fish_greeting
 set -U autovenv_enable yes
 set -U autovenv_announce yes
-set -x HAREPATH /usr/src/hare/stdlib:/usr/src/hare/third-party:/home/roniell/sources/hare-raylib
+
+#set -x HAREPATH /usr/src/hare/stdlib:/usr/src/hare/third-party:/home/roniell/sources/hare-raylib
+
 set -x EDITOR nvim
-
 set -l CODE "/home/roniell/coding/c"
-
 set -Ux C_INCLUDE_PATH  "$CODE/llist/include:$CODE/hashtable/include:$CODE/array_int/include"
 set -Ux LIBRARY_PATH    "$CODE/llist/lib:$CODE/hashtable/lib:$CODE/array_int/lib"
 set -Ux LD_LIBRARY_PATH "$CODE/llist/lib:$CODE/hashtable/lib:$CODE/array_int/lib"
