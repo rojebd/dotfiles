064afa9fb2e76161ee0c61d4a6da7b50412473bd
rojebd <roniellberrios@gmail.com>
2025-02-09 22:21:03 -0700

    Neovim: Telescope change layout config for certain keybinds

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,36 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+
+local function find_files()
+    builtin.find_files({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.5,
+        },
+    })
+end
+
+local function find_buffers()
+    builtin.buffers({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.5,
+        },
+    })
+end
+
+local function find_git_files()
+    builtin.git_files({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.5,
+        },
+    })
+end
+
+vim.keymap.set('n', '<leader>ff', find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fG', builtin.git_files, { desc = 'Telescope git find files' })
+vim.keymap.set('n', '<leader>fb', find_buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fG', find_git_files, { desc = 'Telescope git find files' })
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
