abfe255f6c37ecedd4dec9d31f24f8faa05080b7
rojebd <roniellberrios@gmail.com>
2025-02-09 22:14:04 -0700

    Lualine remove unnecesseary config

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,12 +1,13 @@
-local function lsp_status()
-    local val = require("lsp-status").status()
-    return val
-end
+--local function lsp_status()
+--    local val = require("lsp-status").status()
+--    return val
+--end
 
 require("lualine").setup {
     options = {
         icons_enabled = false,
         -- theme = "gruvbox_dark",
     },
-    sections = { lualine_x = { 'encoding', 'fileformat', 'filetype', lsp_status }, }
+    --sections = { lualine_x = { 'encoding', 'fileformat', 'filetype', lsp_status }, }
+    sections = { lualine_x = { 'encoding', 'fileformat', 'filetype' }, }
 }
