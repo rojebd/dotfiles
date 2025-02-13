77649e936871b620f774d3ef45726d43edd50e17
rojebd <roniellberrios@gmail.com>
2025-02-13 16:48:30 -0700

    Chore

--- .config/nvim/after/plugin/formatting.lua
+++ .config/nvim/after/plugin/formatting.lua
@@ -2,7 +2,7 @@ local conform = require("conform")
 
 conform.setup({
   formatters_by_ft = {
-    c = { "clang-format" },
+    --c = { "clang-format" },
   },
   format_on_save = {
       timeout_ms = 500,
