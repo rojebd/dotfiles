8f4aa81bc50834a08e8d49df0e8f8867d7bc136a
rojebd <roniellberrios@gmail.com>
2025-02-21 15:49:58 -0700

    Neovim: Remove odin treesitter for some reason. It was there

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the listed parsers MUST always be installed)
     -- comment is needed to define custom highlight groups for comments see
     -- below this file for more info
-    ensure_installed = { "comment", "python", "odin", "c", "lua" },
+    ensure_installed = { "comment", "python", "c", "lua" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
